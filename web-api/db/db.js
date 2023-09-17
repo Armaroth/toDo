89ce657c4ee25f637330760e691dbcb630b65cbc
George Spanos <gspanos@moby-it.com>
2023-09-17 12:14:19 +0300

    db setup and some lints

--- web-api/db/db.js
+++ web-api/db/db.js
@@ -1,15 +1,15 @@
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
-    user: 'user-name',
-    password: 'strong-password',
-    host: 'localhost',
-    port: 5432,
-    database: 'todos'
-})
+const pool = new Pool();
 
 async function doQuery(query, args) {
-    await pool.query(query, ...args)
+    await pool.query(query, ...args);
+}
+async function initDb() {
+    await pool.query(`create table if not exists todo (
+        id serial primary key,
+        description text not null 
+    )`);
 }
 
-module.exports = { pool, doQuery };
+module.exports = { pool, doQuery, initDb };
